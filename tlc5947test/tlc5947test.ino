3df9695fa9f0fd0732f8d2ffeea8a2c2b4ceb362
Jaehee Park <jaehee@ramonaoptics.com>
2020-01-08 18:03:43 -0500

    only perform begin when power is plugged in.

--- tlc5947test/tlc5947test.ino
+++ tlc5947test/tlc5947test.ino
@@ -31,6 +31,7 @@
 #define latch6 20
 #define latch7 21
 #define latch8 22
+#define analog_power_check 23
 // set to -1 to not use the enable pin (its optional)
 #define blank  6
 
@@ -47,9 +48,35 @@ Adafruit_TLC5947 tlc = Adafruit_TLC5947(NUM_TLC5947,
   (unsigned char)latch7,
   (unsigned char)latch8);
 
+void wait_until_power_plugged_in(){
+  Serial.begin(115200);
+
+  while(true){
+    // vref value from https://forum.pjrc.com/threads/54662-Teensy-3-2-ADC-Input-Range
+    float v_ref = 3.3;
+    // assumes 10 bit for max count value
+    int val;
+    val = analogRead(9);
+    float analog_power = (val / 1023.0) * v_ref;
+    float v_in = analog_power * 3;
+    Serial.print("analog 9 is: ");
+    Serial.println(val);
+    Serial.print("Voltage is: ");
+    Serial.println(v_in);
+
+    // if ((v_in > 4.0) && (v_in < 5.5)){
+    if ((v_in > 4.0)){
+      // power cable plugged in
+      break;
+     }
+     delay(500);
+  }
+}
 void setup() {
   pinMode(blank, OUTPUT);
   digitalWrite(blank, HIGH);
+  // wait until power plugged in
+  wait_until_power_plugged_in();
   tlc.begin();
   digitalWrite(blank, LOW);
 }
